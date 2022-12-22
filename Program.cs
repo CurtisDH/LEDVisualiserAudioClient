08d6c5d90bbd425a05379674f98448e995ce3ec8
CurtisDH <39945904+CurtisDH@users.noreply.github.com>
2022-12-22 14:39:07 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace AudioClient
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (WasapiLoopbackCapture capture = new())
+            {
+                capture.WaveFormat = new WaveFormat(44100, 16, 2);
+                // Set up an event handler to receive the audio data
+                capture.DataAvailable += (sender, e) =>
+                {
+                    // TODO 
+                    // where do we want to send this, and how do we want to process it? // e.Buffer;
+                };
+
+                // Start capturing audio
+                capture.StartRecording();
+
+                // Wait for the user to stop capturing audio
+                Console.WriteLine("Press any key to stop capturing audio...");
+                Console.ReadKey();
+
+                // Stop capturing audio
+                capture.StopRecording();
+            }
+        }
+    }
+}
