30f2f15efeee117435f39cbe2de054af8502627c
CurtisDH <39945904+CurtisDH@users.noreply.github.com>
2023-01-02 12:13:37 +1000

    - Increased colour range, added 'off' byte array

--- AudClient.cs
+++ AudClient.cs
@@ -11,6 +11,7 @@ public class AudClient
     double[] dataFft;
     private WaveFormat captureWF;
     private string ip;
+    private bool sentRealPacket;
 
     public AudClient(double threshold, string ip)
     {
@@ -104,6 +105,7 @@ public class AudClient
             byte delay = 0;
             // dividing for reduced brightness
             var calc = (byte)(y / 32);
+            // TODO, this is gross
             switch (y / 4)
             {
                 case > 0 and < 21:
@@ -128,85 +130,85 @@ public class AudClient
                     colourB = calc;
 
                     break;
-                case 86:
+                case > 86 and < 107:
                     colourR = 0;
                     colourG = calc;
                     colourB = (byte)(calc / 2);
 
                     break;
-                case 107:
+                case > 107 and <129:
                     colourR = 0;
                     colourG = calc;
                     colourB = (byte)(calc / 1.25);
 
                     break;
-                case 129:
+                case > 129 and < 150:
                     colourR = 0;
                     colourG = calc;
                     colourB = 0;
 
                     break;
-                case 150:
+                case > 150 and < 172:
                     colourR = (byte)(calc / 2);
                     colourG = calc;
                     colourB = 0;
 
                     break;
-                case 172:
+                case > 172 and < 193:
                     colourR = (byte)(calc / 3);
                     colourG = calc;
                     colourB = 0;
 
                     break;
-                case 193:
+                case > 193 and < 215:
                     colourR = calc;
                     colourG = (byte)(calc / 2);
                     colourB = 0;
 
                     break;
-                case 215:
+                case > 215 and < 237:
                     colourR = calc;
                     colourG = (byte)(calc / 3);
                     colourB = 0;
 
                     break;
-                case 237:
+                case > 237 and < 258:
                     colourR = calc;
                     colourG = (byte)(calc / 4);
                     colourB = 0;
 
                     break;
-                case 258:
+                case > 258 and < 280 :
                     colourR = (byte)(calc / 4);
                     colourG = calc;
                     colourB = (byte)(calc / 1.2);
 
                     break;
-                case 280:
+                case > 280 and < 301:
                     colourR = 0;
                     colourG = 0;
                     colourB = calc;
 
                     break;
-                case 301:
+                case > 301 and 323:
                     colourR = 0;
                     colourG = calc;
                     colourB = 0;
 
                     break;
-                case 323:
+                case > 323 and < 344:
                     colourR = 0;
                     colourG = calc;
                     colourB = (byte)(calc / 4);
 
                     break;
-                case 344:
+                case > 344 and < 366:
                     colourR = 0;
                     colourG = calc;
                     colourB = (byte)(calc / 1.25);
 
                     break;
-                case 366:
+                case > 366 and < 400:
                     colourR = 0;
                     colourG = (byte)(calc / 2);
                     colourB = calc;
@@ -218,7 +220,7 @@ public class AudClient
                     colourB = calc;
                     break;
             }
-
+            // TODO send 'off' packet, with a boolean to prevent spam
 
             byte brightness = calc;
             byte numLeds = (byte)(calc / 2);
@@ -227,7 +229,20 @@ public class AudClient
             //Console.WriteLine(y);
             var networkClient = new NetClient(ip, 5555);
             networkClient.SendData(meaningfulData);
+            sentRealPacket = true;
             return;
+        }
+        else if (sentRealPacket)
+        {
+            byte colourR = 0;
+            byte colourG = 0;
+            byte colourB = 0;
+            byte delay = 0;
+            byte[] meaningfulData = new[] { (byte)0, colourR, colourG, colourB, (byte)0, delay };
+            //Console.WriteLine(y);
+            var networkClient = new NetClient(ip, 5555);
+            networkClient.SendData(meaningfulData);
+            sentRealPacket = false;
         }
     }
 }
