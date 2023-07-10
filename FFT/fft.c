b1dac7398e86758c96c22e38ee5cf57f37c303fd
CurtisDH <curtisdh0@gmail.com>
2023-07-10 16:10:48 +1000

    Minor refactor to help with readability

--- FFT/fft.c
+++ FFT/fft.c
@@ -5,13 +5,11 @@
 #include <pulse/simple.h>
 #include <pulse/error.h>
 #include <malloc.h>
-#include <unistd.h>
 #include <pthread.h>
 
 
 #include "../LED/led.h"
 #include "../Configs/constants.h"
-#include "../LED/colour.h"
 
 // TODO major refactor this class is gross
 
@@ -19,6 +17,10 @@ int isRunning = 1;
 
 void *stripUpdateThread(void *arg);
 
+
+int ProcessBufferAndSendData(int16_t *buffer, int buffer_index, Led *LedArray, Colour *prevColour);
+
+int ShiftBuffer(int16_t *buffer, int buffer_index);
 
 void fft(complex double *x, int n)
 {
@@ -83,115 +85,105 @@ void AnalyseAudio(pa_simple *record_handle, int *error, int16_t *buffer, int buf
     Colour prevColour = {0, 0, 0};
     while (1)
     {
-// TODO, allow for the user to interrupt this so they can change device without restart
-// Read audio samples into the circular buffer
+        // TODO, allow for the user to interrupt this so they can change device without restart
+        // Read audio samples into the circular buffer
 
-        if (
-                pa_simple_read(record_handle, buffer
-                                              + buffer_index, (BUFFER_SIZE - buffer_index) * sizeof(int16_t),
-                               error) < 0)
+        if (pa_simple_read(record_handle, buffer + buffer_index,
+                           (BUFFER_SIZE - buffer_index) * sizeof(int16_t), error) < 0)
         {
-            fprintf(stderr,
-                    "Failed to capture audio: %s\n",
-                    pa_strerror((*error))
-            );
+            fprintf(stderr, "Failed to capture audio: %s\n", pa_strerror((*error)));
             break;
         }
 
         buffer_index += (BUFFER_SIZE - buffer_index);
 
-// If enough samples are available, perform FFT and analysis
-        while (buffer_index >= SAMPLE_RESOLUTION)
-        {
-// Convert captured audio buffer to complex double type
-            complex double complex_buffer[SAMPLE_RESOLUTION];
-            for (
-                    int i = 0;
-                    i < SAMPLE_RESOLUTION;
-                    ++i)
-            {
-                complex_buffer[i] = (double) buffer[i] / (double) INT16_MAX;
-            }
-
-// Perform FFT on the captured audio
-            fft(complex_buffer,
-                SAMPLE_RESOLUTION
-            );
-
-// Calculate frequency and magnitude for each sample
-            double frequencies[SAMPLE_RESOLUTION];
-            double magnitudes[SAMPLE_RESOLUTION];
-            double max_magnitude = 0.0;
-            int max_magnitude_index = 0;
-
-            for (
-                    int i = 0;
-                    i < SAMPLE_RESOLUTION;
-                    ++i)
-            {
-                frequencies[i] = (double) SAMPLE_RATE * i /
-                                 SAMPLE_RESOLUTION;
-                magnitudes[i] =
-                        cabs(complex_buffer[i]);
-
-                if (magnitudes[i] > max_magnitude && frequencies[i] < FILTER_MAGNITUDE)
-                {
-                    max_magnitude = magnitudes[i];
-                    max_magnitude_index = i;
-                }
-            }
-            // just avoiding spamming the terminal 
-            if (max_magnitude > 0)
-            {
-                printf("Maximum magnitude: %f at frequency: %f Hz\n", max_magnitude, frequencies[max_magnitude_index]);
-            }
-
-            // Calculate the average magnitude
-            int start = max_magnitude_index - AVERAGE_WINDOW_SIZE / 2;
-            int end = start + AVERAGE_WINDOW_SIZE;
-
-            if (start < 0)
-            {
-                start = 0;
-                end = AVERAGE_WINDOW_SIZE;
-            }
-            if (end > SAMPLE_RESOLUTION)
-            {
-                end = SAMPLE_RESOLUTION;
-                start = end - AVERAGE_WINDOW_SIZE;
-            }
-            double averageMagnitude = calculateAverageMagnitude(magnitudes, start, end);
-            //printf("avg mag %f", averageMagnitude);
-            // couldn't see a difference with this
-//            if (max_magnitude < averageMagnitude)
-//            {
-//                // Shift buffer contents by hop size
-//                memmove(buffer, buffer
-//                                + HOP_SIZE, (BUFFER_SIZE - HOP_SIZE) * sizeof(int16_t));
-//                buffer_index -= HOP_SIZE;
-//                continue;
-//            }
-            Colour *colour = malloc(sizeof(Colour));
-            colour->r = 0;
-            colour->g = 0;
-            colour->b = 0;
-            DetermineColour(colour, frequencies[max_magnitude_index]);
-            Colour blended = ColourBlend(*colour, prevColour);
-            prevColour = blended;
-            AddLed(blended, LedArray, averageMagnitude);
-            free(colour);
-
-// Shift buffer contents by hop size
-            memmove(buffer, buffer
-                            + HOP_SIZE, (BUFFER_SIZE - HOP_SIZE) * sizeof(int16_t));
-            buffer_index -= HOP_SIZE;
-//            usleep(DELAY_IN_MS * 1000); // we dont want delay here, we want it on a diff thread for update
-        }
+        // If enough samples are available, perform FFT and analysis
+        buffer_index = ProcessBufferAndSendData(buffer, buffer_index, LedArray, &prevColour);
     }
-    // Currently no way to exit, but once we do i don't want to have forgotten later
+    // Currently no way to exit, but once we do I don't want to have forgotten later
     free(LedArray);
     isRunning = 0;
     pthread_join(pthread, NULL);
+}
+
+int ProcessBufferAndSendData(int16_t *buffer, int buffer_index, Led *LedArray, Colour *prevColour)
+{
+    while (buffer_index >= SAMPLE_RESOLUTION)
+    {
+// Convert captured audio buffer to complex double type
+        complex double complex_buffer[SAMPLE_RESOLUTION];
+        for (int i = 0; i < SAMPLE_RESOLUTION; ++i)
+        {
+            complex_buffer[i] = (double) buffer[i] / (double) INT16_MAX;
+        }
+
+        // Perform FFT on the captured audio
+        fft(complex_buffer, SAMPLE_RESOLUTION);
+
+        // Calculate frequency and magnitude for each sample
+        double frequencies[SAMPLE_RESOLUTION];
+        double magnitudes[SAMPLE_RESOLUTION];
+        double max_magnitude = 0.0;
+        int max_magnitude_index = 0;
+
+        for (int i = 0; i < SAMPLE_RESOLUTION; ++i)
+        {
+            frequencies[i] = (double) SAMPLE_RATE * i / SAMPLE_RESOLUTION;
+            magnitudes[i] = cabs(complex_buffer[i]);
+
+            if (magnitudes[i] > max_magnitude && frequencies[i] < FILTER_MAGNITUDE)
+            {
+                max_magnitude = magnitudes[i];
+                max_magnitude_index = i;
+            }
+        }
+        // just avoiding spamming the terminal
+        if (max_magnitude > 0)
+        {
+            printf("Maximum magnitude: %f at frequency: %f Hz\n", max_magnitude,
+                   frequencies[max_magnitude_index]);
+        }
+
+        // Calculate the average magnitude
+        int start = max_magnitude_index - AVERAGE_WINDOW_SIZE / 2;
+        int end = start + AVERAGE_WINDOW_SIZE;
+
+        if (start < 0)
+        {
+            start = 0;
+            end = AVERAGE_WINDOW_SIZE;
+        }
+        if (end > SAMPLE_RESOLUTION)
+        {
+            end = SAMPLE_RESOLUTION;
+            start = end - AVERAGE_WINDOW_SIZE;
+        }
+        double averageMagnitude = calculateAverageMagnitude(magnitudes, start, end);
+
+        Colour *colour = malloc(sizeof(Colour));
+        colour->r = 0;
+        colour->g = 0;
+        colour->b = 0;
+        DetermineColour(colour, frequencies[max_magnitude_index]);
+        Colour blended = ColourBlend(*colour, (*prevColour));
+        (*prevColour) = blended;
+        // this adds an led to the strip, and the update thread will display it 
+        // from my testing this delay isn't enough to cause visual delay on critical audio moments e.g. beat drop
+        AddLed(blended, LedArray, averageMagnitude);
+        free(colour);
+
+        buffer_index = ShiftBuffer(buffer, buffer_index);
+    }
+    return buffer_index;
+}
+
+int ShiftBuffer(int16_t *buffer, int buffer_index)
+{
+    // Shift buffer contents by hop size
+    memmove(buffer, buffer
+                    + HOP_SIZE, (BUFFER_SIZE - HOP_SIZE) * sizeof(int16_t));
+    buffer_index -= HOP_SIZE;
+    return buffer_index;
 }
 
 
